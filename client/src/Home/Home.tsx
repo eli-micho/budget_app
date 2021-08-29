91f62e8fb4393a164d6d22ddcf85f055c1fa9ea3
Elijah Micho <olum.micho@gmail.com>
2021-08-29 05:20:39 -0600

    created home login form

--- client/src/Home/Home.tsx
+++ client/src/Home/Home.tsx
@@ -1,9 +1,55 @@
+import { MdAccountBalanceWallet } from 'react-icons/md';
+
 export default function Home() {
     return (
         <div className="home">
             <div className="auth-form">
                 <div className="logo-container">
-                    <span></span>
+                    <span>
+                        <MdAccountBalanceWallet />
+                    </span>
+                    <span>Budgyt</span>
+                </div>
+                <form>
+                    <div className="login-fields">
+                        <div className="login-field-container">
+                            <span></span>
+                            <label>
+                                <input placeholder="Username" />
+                            </label>
+                            <div className="error"></div>
+                        </div>
+
+                        <div className="login-field-container">
+                            <span></span>
+                            <label>
+                                <input placeholder="Password" />
+                            </label>
+                            <div className="error"></div>
+                        </div>
+                    </div>
+                    <div className="sign-up-container">
+                        <span>
+                            Don&#39;t have an account? <button>Sign Up!</button>
+                        </span>
+                    </div>
+                    <div className="login-buttons">
+                        <button type="submit">Login</button>
+                        <button className="sample-account">
+                            Sample Account
+                        </button>
+                    </div>
+                </form>
+                <div className="social-media-container">
+                    <div className="social-media-title">
+                        <hr />
+                        Login with social media
+                        <hr />
+                    </div>
+                    <div className="social-media-login">
+                        <div className="google">Google</div>
+                        <div className="google">Google</div>
+                    </div>
                 </div>
             </div>
         </div>
